ebe2e890a4c7c51bb06e769d76f989e346bd2b99
Eugene Belkovich <eugene.belkovich@gmail.com>
2023-04-04 18:07:23 +0200

    feat: - seed script added - seed data added - mongo-seeding installed

---
+++ seed/data/2-instances/instances.js
@@ -0,0 +1,22 @@
+const instances = [
+  {
+    "id": "e335175a-eace-4a74-b99c-c6466b6afadd",
+    "group": "particle-detector-1",
+    "createdAt": 1571418096158,
+    "updatedAt": 1571418124127,
+    "meta": {
+      "foo": 1
+    }
+  },
+  {
+    "id": "e335175a-eace-4a74-b99c-c6466b6afacc",
+    "group": "particle-detector-2",
+    "createdAt": 1571418096158,
+    "updatedAt": 1571418124127,
+    "meta": {
+      "foo": 2
+    }
+  },
+];
+
+module.exports = instances;
