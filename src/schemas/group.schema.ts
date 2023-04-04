6fa60a86abf2167ad2d5919a4103521759feef82
Eugene Belkovich <eugene.belkovich@gmail.com>
2023-04-04 18:11:10 +0200

    feat: - group and instance schemas added

---
+++ src/schemas/group.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Group {
+  @Prop({ required: true })
+  group: string;
+
+  @Prop({ required: true })
+  instances: string;
+
+  @Prop({ required: true })
+  createdAt: number;
+
+  @Prop({ required: true })
+  lastUpdatedAt: number;
+}
+
+export const GroupSchema = SchemaFactory.createForClass(Group);
+
+export type GroupDocument = Group & Document;
---
+++ src/schemas/instance.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Instance extends Document {
+  @Prop({ required: true })
+  id: string;
+
+  @Prop({ required: true })
+  group: string;
+
+  @Prop({ required: true })
+  createdAt: number;
+
+  @Prop({ required: true })
+  updatedAt: number;
+
+  @Prop({ type: Object })
+  meta?: Record<string, any>;
+}
+
+export const InstanceSchema = SchemaFactory.createForClass(Instance);
+
+export type InstanceDocument = Instance & Document;
+
